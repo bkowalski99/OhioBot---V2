7ebf0ee7ba69948ae89a8ce2611c24964be9235a
Ben Kowalski <50568067+bkowalski99@users.noreply.github.com>
2023-12-12 22:02:13 -0500

    Update discordBot.py
    updating for web version

--- discordBotCode/discordBot.py
+++ discordBotCode/discordBot.py
@@ -11,10 +11,10 @@ from textblob import TextBlob
 import time
 
 #consts
-reader = open(r"C:\Users\bkowa\Documents\Python Code\OhioBot - V2\OhioBot---V2\discordBotCode\SecretKeyURI.txt", "r")
+reader = open(r"home\bkowalski99\ohiobot\OhioBot---V2\discordBotCode\SecretKeyURI.txt", "r")
 uri = reader.readline()
 print(uri)
-cryingEmoji =  r"C:\Users\bkowa\Documents\Python Code\OhioBot - V2\OhioBot---V2\discordBotCode\crying-emoji-dies.gif"
+cryingEmoji =  r"home\bkowalski99\ohiobot\OhioBot---V2\discordBotCode\crying-emoji-dies.gif"
 intents = discord.Intents.default()
 intents.message_content = True
 jaredHate = False
