58c7d29f9af72ffa12e21d63ac7cd86326b72342
Ben Kowalski <50568067+bkowalski99@users.noreply.github.com>
2024-01-04 15:32:56 -0500

    Update mongoTesting.py

--- mongoTesting.py
+++ mongoTesting.py
@@ -1,7 +1,7 @@
 from pymongo.server_api import ServerApi
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://benDiscord:Gh8NBMxt0w0Xx2P0@discordbot.trkl0dk.mongodb.net/?retryWrites=true&w=majority"
+uri = ""
 
 client = MongoClient(uri, server_api = ServerApi("1"))
 db = client["HHGInfo"]
